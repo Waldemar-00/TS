904d39cc8c278f83fb412930bbc2575f5cda6814
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-09-29 15:06:45 +0300

    ts-node file.ts, tsc file.ts, node file.js, TS will never changes JS.

---
+++ index.js
@@ -0,0 +1,4 @@
+var str = 'hello';
+console.log(str);
+str = 5; //Code will work without Error, because TS never changes JS
+console.log(str);
---
+++ index.ts
@@ -0,0 +1,4 @@
+let  str: string = 'hello'
+console.log( str )
+str = 5; //TS will show us Error
+console.log(str)
