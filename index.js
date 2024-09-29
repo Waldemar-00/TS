998c9b5ae50eb9274160f991b7b181eb30a31d49
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-09-29 16:29:24 +0300

    TS/function(): void | string.

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-var str = 'hello';
-console.log(str);
-str = 5; //Code will work without Error, because TS never changes JS
-console.log(str);
+var str = 'Happy birthday!';
+var errorMessage = 'Something wrong!';
+var isBirthday = true;
+var age = 42;
+var greeting = 'Hello';
+function logCongrats(str, isBirthday, age, errorMessage) {
+    if (isBirthday) {
+        console.log("".concat(str, " Your age is ").concat(age + 1));
+    }
+    else
+        console.log(errorMessage);
+    return undefined;
+}
+console.log(logGreeting(true, greeting));
+logCongrats('some', undefined, 101, 'Something wrong :-(!');
+function logGreeting(isBirthday, greeting) {
+    if (isBirthday)
+        return greeting + '!';
+}
--- index.ts
+++ index.ts
@@ -1,4 +1,22 @@
-let  str: string = 'hello'
-console.log( str )
-str = 5; //TS will show us Error
-console.log(str)
+let str: string = 'Happy birthday!'
+const errorMessage = 'Something wrong!'
+let isBirthday: boolean = true
+let age: number = 42
+let greeting: string = 'Hello'
+
+function logCongrats ( str: string, isBirthday: boolean | null | undefined, age: number, errorMessage: string ): void
+{
+  if ( isBirthday )
+{
+  console.log(`${str} Your age is ${age + 1}`);
+
+  } else console.log(errorMessage);
+return undefined
+}
+console.log(logGreeting(true, greeting))
+logCongrats( 'some', undefined, 101, 'Something wrong :-(!' )
+
+function logGreeting ( isBirthday: boolean | null | undefined, greeting: string ): string | void
+{
+  if(isBirthday) return greeting + '!'
+}
