620bc48a607439aa03720ed63772a0797e40fd34
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-09-30 21:25:12 +0300

    TS types: bigint, symbol.

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var userData = '{"str": "Happy birthday!", "errorMessage": "Something wrong!", "isBirthday":true,"age":42,"greeting":"Hello", "name": "John"}';
-var user = JSON.parse(userData);
-console.log(user);
-var str = 'Happy birthday!';
-var errorMessage = 'Something wrong!';
-var isBirthday = true;
-var age = 42;
-var greeting = 'Hello';
-function logCongrats(str, isBirthday, age, errorMessage) {
-    if (isBirthday) {
-        console.log("".concat(str, " Your age is ").concat(age + 1));
-    }
-    else
-        console.log(errorMessage);
-    return undefined;
-}
-console.log(logGreeting(true, greeting));
-logCongrats('some', undefined, 101, 'Something wrong :-(!');
-var createError = function (msg) {
-    throw new Error();
-};
-function logGreeting(isBirthday, greeting) {
-    if (isBirthday)
-        return greeting + '!';
-    else
-        return createError('Something went wrong!');
-}
+// let userData = '{"str": "Happy birthday!", "errorMessage": "Something wrong!", "isBirthday":true,"age":42,"greeting":"Hello", "name": "John"}'
+// let user: {
+//   str: string,
+//   errorMessage: string,
+//   isBirthday: boolean,
+//   age: number,
+//   greeting: string,
+//   name: string
+// } = JSON.parse( userData )
+// console.log(user);
+var _a;
+// let str: string = 'Happy birthday!'
+// const errorMessage = 'Something wrong!'
+// let isBirthday: boolean = true
+// let age: number = 42
+// let greeting: string = 'Hello'
+// function logCongrats ( str: string, isBirthday: boolean | null | undefined, age: number, errorMessage: string ): void
+// {
+//   if ( isBirthday )
+// {
+//   console.log(`${str} Your age is ${age + 1}`);
+//   } else console.log(errorMessage);
+// return undefined
+// }
+// console.log(logGreeting(true, greeting))
+// logCongrats( 'some', undefined, 101, 'Something wrong :-(!' )
+// const createError = (msg: string): never =>
+// {
+//   throw new Error()
+// }
+// function logGreeting ( isBirthday: boolean | null | undefined, greeting: string ): string | never
+// {
+//   if ( isBirthday ) return greeting + '!'
+//   else return createError('Something went wrong!')
+// }
 //null, undefined, void
 var someNull = null;
 var someNullValue = null;
@@ -37,28 +43,38 @@ var action = function () { };
 var any = action();
 var v = action();
 //never
-function nevering() {
-    throw new Error();
-}
-var fromNevering = nevering();
-var fromNeveringAny = nevering();
-var number = nevering();
-var string = nevering();
-var boolean = nevering();
-var undefined_ = nevering();
-var null_ = nevering();
-var void_ = nevering();
-//unknown
-var num = 10;
-var str_1 = 'some';
-var bool = false;
-var unknown_5 = 11;
-var unknown = nevering();
-var unknown_ = undefined;
-var unknown_1 = null;
-var unknown_2 = any;
-var unknown_3 = v;
-var unknown_num = num;
-var unknown_str = str_1;
-var unknown_bool = bool;
-var unknown_unknown = unknown_5;
+// function nevering (): never //!always need to assign or you get type: void
+// {
+//   throw new Error()
+// }
+// const fromNevering: never = nevering()
+// const fromNeveringAny: any = nevering()
+// const number: number = nevering()
+// const string: string = nevering()
+// const boolean: boolean = nevering()
+// const undefined_: undefined = nevering()
+// const null_: null = nevering()
+// const void_: void = nevering()
+// //unknown
+// const num: number = 10
+// const str_1: string = 'some'
+// const bool: boolean = false
+// let unknown_5: unknown = 11
+// const unknown: unknown = nevering()
+// const unknown_: unknown = undefined
+// const unknown_1: unknown = null
+// const unknown_2: unknown = any
+// const unknown_3: unknown = v
+// const unknown_num: unknown = num
+// const unknown_str: unknown = str_1
+// const unknown_bool: unknown = bool
+// const unknown_unknown: unknown = unknown_5
+//Symbol
+var sym = Symbol('sym');
+var data = (_a = {},
+    _a[sym] = 100,
+    _a);
+console.log(data[sym]);
+//BigInt
+var bigint = BigInt(111);
+console.log(bigint);
--- index.ts
+++ index.ts
@@ -1,42 +1,40 @@
-import { create } from "domain";
+// let userData = '{"str": "Happy birthday!", "errorMessage": "Something wrong!", "isBirthday":true,"age":42,"greeting":"Hello", "name": "John"}'
+// let user: {
+//   str: string,
+//   errorMessage: string,
+//   isBirthday: boolean,
+//   age: number,
+//   greeting: string,
+//   name: string
+// } = JSON.parse( userData )
+// console.log(user);
 
-let userData = '{"str": "Happy birthday!", "errorMessage": "Something wrong!", "isBirthday":true,"age":42,"greeting":"Hello", "name": "John"}'
-let user: {
-  str: string,
-  errorMessage: string,
-  isBirthday: boolean,
-  age: number,
-  greeting: string,
-  name: string
-} = JSON.parse( userData )
-console.log(user);
+// let str: string = 'Happy birthday!'
+// const errorMessage = 'Something wrong!'
+// let isBirthday: boolean = true
+// let age: number = 42
+// let greeting: string = 'Hello'
 
-let str: string = 'Happy birthday!'
-const errorMessage = 'Something wrong!'
-let isBirthday: boolean = true
-let age: number = 42
-let greeting: string = 'Hello'
+// function logCongrats ( str: string, isBirthday: boolean | null | undefined, age: number, errorMessage: string ): void
+// {
+//   if ( isBirthday )
+// {
+//   console.log(`${str} Your age is ${age + 1}`);
 
-function logCongrats ( str: string, isBirthday: boolean | null | undefined, age: number, errorMessage: string ): void
-{
-  if ( isBirthday )
-{
-  console.log(`${str} Your age is ${age + 1}`);
-
-  } else console.log(errorMessage);
-return undefined
-}
-console.log(logGreeting(true, greeting))
-logCongrats( 'some', undefined, 101, 'Something wrong :-(!' )
-const createError = (msg: string): never =>
-{
-  throw new Error()
-}
-function logGreeting ( isBirthday: boolean | null | undefined, greeting: string ): string | never
-{
-  if ( isBirthday ) return greeting + '!'
-  else return createError('Something went wrong!')
-}
+//   } else console.log(errorMessage);
+// return undefined
+// }
+// console.log(logGreeting(true, greeting))
+// logCongrats( 'some', undefined, 101, 'Something wrong :-(!' )
+// const createError = (msg: string): never =>
+// {
+//   throw new Error()
+// }
+// function logGreeting ( isBirthday: boolean | null | undefined, greeting: string ): string | never
+// {
+//   if ( isBirthday ) return greeting + '!'
+//   else return createError('Something went wrong!')
+// }
 
 
 //null, undefined, void
@@ -54,31 +52,47 @@ const v: void = action()
 
 //never
 
-function nevering (): never //!always need to assign or you get type: void
-{
-  throw new Error()
+// function nevering (): never //!always need to assign or you get type: void
+// {
+//   throw new Error()
+// }
+
+// const fromNevering: never = nevering()
+// const fromNeveringAny: any = nevering()
+// const number: number = nevering()
+// const string: string = nevering()
+// const boolean: boolean = nevering()
+// const undefined_: undefined = nevering()
+// const null_: null = nevering()
+// const void_: void = nevering()
+
+// //unknown
+// const num: number = 10
+// const str_1: string = 'some'
+// const bool: boolean = false
+// let unknown_5: unknown = 11
+// const unknown: unknown = nevering()
+// const unknown_: unknown = undefined
+// const unknown_1: unknown = null
+// const unknown_2: unknown = any
+// const unknown_3: unknown = v
+// const unknown_num: unknown = num
+// const unknown_str: unknown = str_1
+// const unknown_bool: unknown = bool
+// const unknown_unknown: unknown = unknown_5
+
+
+//Symbol
+
+const sym: symbol = Symbol( 'sym' )
+
+const data = {
+  [ sym ]: 100
 }
 
-const fromNevering: never = nevering()
-const fromNeveringAny: any = nevering()
-const number: number = nevering()
-const string: string = nevering()
-const boolean: boolean = nevering()
-const undefined_: undefined = nevering()
-const null_: null = nevering()
-const void_: void = nevering()
+console.log(data[sym]);
 
-//unknown
-const num: number = 10
-const str_1: string = 'some'
-const bool: boolean = false
-let unknown_5: unknown = 11
-const unknown: unknown = nevering()
-const unknown_: unknown = undefined
-const unknown_1: unknown = null
-const unknown_2: unknown = any
-const unknown_3: unknown = v
-const unknown_num: unknown = num
-const unknown_str: unknown = str_1
-const unknown_bool: unknown = bool
-const unknown_unknown: unknown = unknown_5
+//BigInt
+
+const bigint: bigint = BigInt( 111 )
+console.log(bigint);
