9261f432b42f6f996550f3c733e57885c9186861
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-10-29 17:06:55 +0300

    IndexAccessTypes and Task JS

--- tasks/gettingTypes/index.js
+++ tasks/gettingTypes/index.js
@@ -1,3 +1,14 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var phones = [
     {
         company: "Nokia",
@@ -22,8 +33,14 @@ var phones = [
     },
 ];
 function filterPhonesByDate(phones, manufactured, initial) {
-    var phArr = phones.filter( ( obj ) => new Date( obj.manufactured ).getTime() > new Date( initial ).getTime()
-    )
-    return phArr;
+    var iPhoneManufacturedAfterDate = phones
+        .filter(function (obj) { return new Date(obj[manufactured]).getTime() > new Date(initial).getTime(); })
+        .map(function (obj) {
+        var objForPhArr = __assign(__assign({}, obj), { initialDate: initial });
+        return objForPhArr;
+    });
+    return iPhoneManufacturedAfterDate;
 }
+// Второй аргумент при вызове функции должен быть связан с первым,
+// а значит мы получим подсказки - свойства этого объекта
 console.log(filterPhonesByDate(phones, "manufactured", "2022-01-01"));
