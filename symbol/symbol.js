e92935ea2c6dd62e7717281f38fa64f625810612
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-11-27 20:46:47 +0300

    experimentalDecorators: false

---
+++ symbol/symbol.js
@@ -0,0 +1,14 @@
+"use strict";
+let symbol = Symbol();
+const user_777 = {
+    name: 'Roman',
+    age: 26,
+    [symbol]: ''
+};
+user_777[symbol] = 'little secret';
+console.log(user_777[symbol]);
+let globalSymbol_777 = Symbol.for('GLOBAL');
+user_777[globalSymbol_777] = 'global SECRET';
+console.log(user_777[globalSymbol_777]);
+const GSymbol = Symbol.for('GLOBAL');
+console.log(GSymbol === globalSymbol_777);
